88afe0ecb7a6c21e40292bb768def990b53d1b0d
Omer Karakas <karakasomer@gmail.com>
2022-05-17 19:44:02 +0300

    robofriends, last touch

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 
@@ -43,7 +44,9 @@ class App extends Component {
         <SearchBox searchChange={this.onSearchChange} />
         <hr />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
