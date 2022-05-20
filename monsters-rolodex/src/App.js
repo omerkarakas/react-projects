b22b3536d0715bb9717ec33e248320d88b7f677a
Omer Karakas <karakasomer@gmail.com>
2022-05-20 17:28:55 +0300

    functional component conversion started

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,80 +1,115 @@
 import logo from './logo.svg';
 
-import { Component } from 'react';
+//import { Component } from 'react';
+import { useState } from 'react';
+
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
-//function App() {
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      //monstersShown: []
-      searchString: ""
-    }
-    //console.log("appjs constructor");
+
+const App = () => {
+  console.log("render");
+  const [searchString, setSearchString] = useState(''); //[value, setValue]
+  console.log(searchString);
+
+  const onSearchChange = (e) => {
+    const searchStringVal = e.target.value.toUpperCase();
+    setSearchString(searchStringVal);
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users =>
-        this.setState(
-          () => {
-            return { monsters: users };
-            // return { monsters: users, monstersShown: users };
-          }
-          // ,
-          // () => {
-          //   console.log(this.state);
-          // }
-        )
-      );
 
-    //  console.log("appjs compDidMount");
 
-  }
-
-  onSearchChange = (e) => {
-    // console.log(e);
-    const searchString = e.target.value.toUpperCase();
-    this.setState(
-      () => {
-        return { searchString };
-      });
-  }
-
-  render() {
-    const { monsters, searchString } = this.state;
-    const { onSearchChange } = this;
-
-    //console.log("appjs render");
-
-    const selectedMons =
-      monsters.filter((mon) => {
-        return mon.name.toUpperCase().includes(searchString)
-      });
-
-    return (
-      <div className="App" >
-        <h1>Fabulous Monsters</h1>
-        <SearchBox
-          className="monsters-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        {/* {
+  return (
+    <div className="App" >
+      <h1>Fabulous Monsters</h1>
+      {<SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      /*
+      {
           selectedMons.map((mon) => {
             return <h1 key={mon.id}>{mon.name}</h1>;
           })
-        } */}
-        <CardList monsters={selectedMons} />
-      </div >
-    );
-  }
+        }
+      <CardList monsters={selectedMons} /> */}
+    </div >
+
+  );
+
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       //monstersShown: []
+//       searchString: ""
+//     }
+//     //console.log("appjs constructor");
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(users =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//             // return { monsters: users, monstersShown: users };
+//           }
+//           // ,
+//           // () => {
+//           //   console.log(this.state);
+//           // }
+//         )
+//       );
+
+//     //  console.log("appjs compDidMount");
+
+//   }
+
+//   onSearchChange = (e) => {
+//     // console.log(e);
+//     const searchString = e.target.value.toUpperCase();
+//     this.setState(
+//       () => {
+//         return { searchString };
+//       });
+//   }
+
+//   render() {
+//     const { monsters, searchString } = this.state;
+//     const { onSearchChange } = this;
+
+//     //console.log("appjs render");
+
+//     const selectedMons =
+//       monsters.filter((mon) => {
+//         return mon.name.toUpperCase().includes(searchString)
+//       });
+
+//     return (
+//       <div className="App" >
+//         <h1>Fabulous Monsters</h1>
+//         <SearchBox
+//           className="monsters-search-box"
+//           onChangeHandler={onSearchChange}
+//           placeholder="search monsters"
+//         />
+//         {/* {
+//           selectedMons.map((mon) => {
+//             return <h1 key={mon.id}>{mon.name}</h1>;
+//           })
+//         } */}
+//         <CardList monsters={selectedMons} />
+//       </div >
+//     );
+//   }
+// }
 
 export default App;
