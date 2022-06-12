d2bd7720c20af0e02ae377ac1518d5b02369a96b
Omer Karakas <karakasomer@gmail.com>
2022-06-12 21:29:40 +0300

    Rating component, add, delete operations added

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
@@ -7,6 +8,11 @@ import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedBack) => {
+    newFeedBack.id = uuidv4();
+    setFeedback([newFeedBack, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback ? ')) {
@@ -17,7 +23,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
