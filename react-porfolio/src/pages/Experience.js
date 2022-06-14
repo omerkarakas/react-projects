a611a0ebc9f965db92c6b3aa633a2a434e1d7135
Omer Karakas <karakasomer@gmail.com>
2022-06-14 23:31:42 +0300

    experience page and some styling issues

--- react-porfolio/src/pages/Experience.js
+++ react-porfolio/src/pages/Experience.js
@@ -1,5 +1,138 @@
+import React from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
+
+import '../styles/Experience.css';
+
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="darkblue">
+        {/* TKK */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="2019 - 2021"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Agricultural Credit Cooperatives, Ankara, Turkey
+          </h3>
+          <h5 className="vertical-timeline-element-subtitle subtitle">
+            Operations / Project Manager
+          </h5>
+          <p>...</p>
+        </VerticalTimelineElement>
+
+        {/* Bilge Adam */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="2019 - 2021"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Bilge Adam Academy, Ankara, Turkey
+          </h3>
+          <h5 className="vertical-timeline-element-subtitle subtitle">
+            Java Instructor
+          </h5>
+          <p>Java, Hibernate, Spring, MVC, AOP, REST, Springboot</p>
+        </VerticalTimelineElement>
+
+        {/* SGK */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="2017 - 2019"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Social Security Institution, Ankara, Turkey
+          </h3>
+          <h5 className="vertical-timeline-element-subtitle subtitle">
+            Database Performance Tuning Expert
+          </h5>
+          <p>IBM DB2, SQL/PL</p>
+        </VerticalTimelineElement>
+
+        {/* PIRIGO */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="2016 - 2017"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Pirigo Payment Solutions, Ankara, Turkey
+          </h3>
+          <h5 className="vertical-timeline-element-subtitle subtitle">
+            Co-Founder, Java/JavaScript Developer
+          </h5>
+          <p>JS, AngularJS, Ionic Framework, Cordova, Vaadin </p>
+        </VerticalTimelineElement>
+
+        {/* INNOVA */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="2004 - 2016"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Innova IT Solutions, Ankara, Turkey
+          </h3>
+          <h5 className="vertical-timeline-element-subtitle subtitle">
+            Java Developer & Oracle Developer/DBA
+          </h5>
+        </VerticalTimelineElement>
+
+        {/* MS */}
+        <VerticalTimelineElement
+          className="vertical-timeline--education"
+          date="2003 - 2006"
+          iconStyle={{ background: '#f5d20b', color: '#fff' }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Middle East Technical University, Ankara, Turkey
+          </h3>
+          <p>MS in Computer Engineering, GPA:3.45</p>
+        </VerticalTimelineElement>
+
+        {/* DOCUART */}
+        <VerticalTimelineElement
+          className="vertical-timeline--work"
+          date="1999 - 2003"
+          iconStyle={{ background: 'steelblue', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Docuart IT, Ankara, Turkey
+          </h3>
+          <p>Oracle Forms/Reports Developer</p>
+        </VerticalTimelineElement>
+
+        {/* BS */}
+        <VerticalTimelineElement
+          className="vertical-timeline--education"
+          date="1994 - 1999"
+          iconStyle={{ background: '#f5d20b', color: '#fff' }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title title">
+            Middle East Technical University, Ankara, Turkey
+          </h3>
+          <p>BS in Civil Engineering, GPA:2.85</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
--- react-porfolio/src/pages/Home.js
+++ react-porfolio/src/pages/Home.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import '../styles/Home.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>
+          Hi, My name is <br /> Omer Karakas
+        </h2>
+        <div className="prompt">
+          <p>
+            I am a software engineer who loves learning, coding and creating
+          </p>
+          <div className="icons-container">
+            <LinkedInIcon />
+            <EmailIcon />
+            <GitHubIcon />
+          </div>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>HTML, CSS, JavaScript, BootStrap, ReactJS, Redux</span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS, Java Spring</span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, C, C++</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
