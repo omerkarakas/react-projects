e255bba03107b21c85352b73834eee44f23024e8
Omer Karakas <karakasomer@gmail.com>
2022-06-16 21:10:05 +0300

    About, NotFound, Search results pages, plus first rest api call added

---
+++ github-profiles/src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Profiles</h1>
+      <p className="text-lg text-gray-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odio ad
+        id dolorem laboriosam fugiat facilis deserunt quae delectus ipsam.
+      </p>
+      <p className="text-lg text-gray-400">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores quas
+        voluptas eveniet asperiores exercitationem architecto beatae sapiente
+        provident error vero, at fugit nesciunt, aspernatur voluptatem quo ipsa
+        ut ratione voluptates.
+      </p>
+      <br />
+      <p>
+        Version: <span className="text-bold">1.0.2</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
