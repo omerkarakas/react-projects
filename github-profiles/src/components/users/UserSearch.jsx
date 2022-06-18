8c8a6727347d42adf66c33749eb405ae6b14b949
Omer Karakas <karakasomer@gmail.com>
2022-06-18 13:19:32 +0300

    Alert component added

--- github-profiles/src/components/users/UserSearch.jsx
+++ github-profiles/src/components/users/UserSearch.jsx
@@ -1,4 +1,5 @@
 import { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
 import GithubContext from '../../context/github/GithubContext';
 
 function UserSearch() {
@@ -6,11 +7,14 @@ function UserSearch() {
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
 
+  const { setAlert } = useContext(AlertContext);
+
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      alert('Please enter something');
+      //alert('Please enter something');
+      setAlert('Please enter something', 'error');
       return;
     }
 
