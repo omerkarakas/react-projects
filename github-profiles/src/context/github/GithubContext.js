ade4a4c7b9d44d385d37e3dea09104f7bd916be5
Omer Karakas <karakasomer@gmail.com>
2022-06-18 11:25:32 +0300

    user search & clear results added

--- github-profiles/src/context/github/GithubContext.js
+++ github-profiles/src/context/github/GithubContext.js
@@ -14,16 +14,25 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' });
+  };
+
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const { items } = await response.json();
 
-    dispatch({ type: 'GET_USERS', payload: data });
+    dispatch({ type: 'GET_USERS', payload: items });
   };
 
   const setLoading = () => {
@@ -32,7 +41,12 @@ export const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
