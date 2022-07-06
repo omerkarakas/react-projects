8f8e354407566d1d8e60deb457c4c7fd35b4d4c9
Omer Karakas <karakasomer@gmail.com>
2022-07-06 17:35:32 +0300

    coctails app started

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea maxime nam
+        aspernatur distinctio, cum sunt facere odio quo praesentium! Esse.
+      </p>
+      <p>
+        Sit porro harum necessitatibus eaque, hic quaerat ipsa repudiandae
+        repellat obcaecati voluptatibus ad atque temporibus aliquam nisi
+        explicabo quibusdam beatae amet omnis iste eum tenetur similique. Nihil
+        accusantium eos dolore corrupti incidunt harum totam recusandae sunt
+        illo soluta voluptatem, assumenda ad expedita, sint dolorem eaque
+        molestias amet magnam? Recusandae fuga ipsa reprehenderit aspernatur in,
+        iusto dicta ad quidem mollitia magnam quos officiis, laboriosam totam,
+        perferendis aut ullam eligendi explicabo provident. Dolorum possimus
+        sint ex.
+      </p>
+    </section>
+  );
+};
+
+export default About;
