89c375031cfaccd26b4cbd001172929b77ab2b09
Omer Karakas <karakasomer@gmail.com>
2022-07-11 19:38:54 +0300

    Router-dom routing added

--- navbar-template/src/App.js
+++ navbar-template/src/App.js
@@ -3,30 +3,36 @@ import Navbar from './Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Pricing from './pages/Pricing';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  let Component;
-  // or let component;
-  console.log(window.location.pathname);
-  switch (window.location.pathname) {
-    case '/':
-      Component = Home;
-      //component = <Home/>
-      break;
-    case '/pricing':
-      Component = Pricing;
-      break;
-    case '/about':
-      Component = About;
-      break;
-    default:
-      break;
-  }
+  // let Component;
+  // // or let component;
+  // console.log(window.location.pathname);
+  // switch (window.location.pathname) {
+  //   case '/':
+  //     Component = Home;
+  //     //component = <Home/>
+  //     break;
+  //   case '/pricing':
+  //     Component = Pricing;
+  //     break;
+  //   case '/about':
+  //     Component = About;
+  //     break;
+  //   default:
+  //     break;
+  // }
   return (
     <>
       <Navbar />
       <div className="container">
-        <Component />
+        {/* <Component /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
 
       {/* {component} */}
